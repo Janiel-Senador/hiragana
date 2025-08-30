79201daad390bd776f906f93855b46640892f442
Janiel Senador <154493279+Janiel-Senador@users.noreply.github.com>
2025-08-30 21:21:07 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-
 import React, { useEffect } from 'react';
 
 const FloatingBubble = ({ delay = 0, duration = 8, onPop }) => {
