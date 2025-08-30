0d769112b671f1c94a2387928f96ab893b3afb74
Janiel Senador <154493279+Janiel-Senador@users.noreply.github.com>
2025-08-30 21:15:27 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
+
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 
 const FloatingBubble = ({ delay = 0, duration = 8, onPop }) => {
   const [position, setPosition] = useState({
